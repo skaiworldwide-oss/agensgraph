85e85ca6a3885309518b4b6af59a0e6cb654cd99
Muhammad Taha Naveed <m.taha.naveed27@gmail.com>
2025-03-13 15:58:10 +0500

    fix: _ as variable name causes conflict
    - Resolves AG-339

--- src/backend/parser/parse_graph.c
+++ src/backend/parser/parse_graph.c
@@ -49,11 +49,6 @@
 #include "catalog/ag_vertex_d.h"
 #include "catalog/ag_edge_d.h"
 
-#define CYPHER_SUBQUERY_ALIAS	"_"
-#define CYPHER_OPTMATCH_ALIAS	"_o"
-#define CYPHER_MERGEMATCH_ALIAS	"_m"
-#define CYPHER_DELETEJOIN_ALIAS	"_d"
-
 #define EDGE_UNION_START_ID		"_start"
 #define EDGE_UNION_END_ID		"_end"
 
